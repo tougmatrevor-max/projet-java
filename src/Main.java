bdc8260b0a666d7784bc5e442cc4d26acbf7faab
tougmatrevor-max <tougmatrevor@gmail.com>
2026-09-09 02:09:28 +0000

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,36 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));qsxffggh
-
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
-    }
-}
 public class Main {
     public static void main(String[] args) {
-        // création des livres on a crere 4 livres
-        Livre livre1 = new Livre();
-        livre1.titre = "Le Bout de boit de Dieu";
-        livre1.auteur = "Sembene Ousmane";
-        livre1.disponible = true;
-        Livre livre2 = new Livre();
-        livre2.titre = "L'enfant noir";
-        livre2.auteur = "Camara Laye";
-        livre2.disponible = false;
-        Livre livre3 = new Livre();
-        livre3.titre = "L'Étranger";
-        livre3.auteur = "Albert Camus";
-        livre3.disponible = true;
-        Livre livre4 = new Livre();
-        livre4.titre = "Candide";
-        livre4.auteur = "Voltaire";
-        livre4.disponible = false;
-        // ici nous avons l'affichage des titre plus l'état de disponibilité des livres
-        System.out.println(livre1.titre + " - " + livre1.disponible);
-        System.out.println(livre2.titre + " - " + livre2.disponible);
-        System.out.println(livre3.titre + " - " + livre3.disponible);
-        System.out.println(livre4.titre + " - " + livre4.disponible);
+        
+        Bibliotheque biblio = new Bibliotheque();
+
+     
+        Document doc1 = new Document(101, "Le Petit Prince", "Antoine de Saint-Exupéry");
+        Document doc2 = new Document(102, "L'Étranger", "Albert Camus");
+
+        biblio.ajouterDocument(doc1);
+        biblio.ajouterDocument(doc2);
+
+       
+        Adherent adh1 = new Adherent(1, "Jean Dupont", "jean.dupont@email.com");
+        Adherent adh2 = new Adherent(2, "Alice Martin", "alice.martin@email.com");
+
+        biblio.ajouterAdherent(adh1);
+        biblio.ajouterAdherent(adh2);
+
+      
+        biblio.afficherCatalogue();
+
+       
+        System.out.println("\n--- OPÉRATIONS ---");
+        biblio.emprunterDocument(101, 1); // Jean emprunte Le Petit Prince
+        biblio.emprunterDocument(101, 2); // Alice tente d'emprunter le même document (doit échouer)
+
+        
+        biblio.afficherCatalogue();
+
+      
+        biblio.retournerDocument(101); // Retour du livre
+
+        biblio.afficherCatalogue();
     }
 }
