dbaca50797043f461f5675de97b1f4960d0f3764
tougmatrevor-max <tougmatrevor@gmail.com>
2026-09-12 02:26:07 +0000

    Modification du Livre.java

--- Version finale/Livre.java
+++ Version finale/Livre.java
@@ -1,18 +1,20 @@
-public class Livre extends Document{
-  private int nombrePages; // Creation de la classe
+public class Livre extends Document {
+    private int nombrePages;
 
-public Livre(int numero, String titre, String auteurPrincipal,int nombrePages){
-  super(numero,titre,auteurPrincipal);
-  if (nombrePages<=0)throw new IllegalArgumentException("Nombres de pages invalide.");
-  this.nombrePages=nombrePages;
-}
-  public int getNombrePages(){return nombrePages;}
-  @Override
-  public int dureeMaxPret(){
-    return 21;
-      }
-  @Override
-  public String toString(){
-    return super.toString()+" Livre( "+ nombrePages +" pages) ";
-  }
+    public Livre(int numero, String titre, String auteurPrincipal, int nombrePages) {
+        super(numero, titre, auteurPrincipal);
+        this.nombrePages = nombrePages;
+    }
+
+    public int getNombrePages() { return nombrePages; }
+
+    @Override
+    public int dureeMaxPret() {
+        return 21;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " - Livre (" + nombrePages + " pages)";
+    }
 }
